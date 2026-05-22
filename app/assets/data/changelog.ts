0ae367acf016bf010689496ad2f305a5c5d81669
mr-calculator <mr-calculator.estimate510@slmails.com>
2026-05-22 19:44:32 +0300

    Added credit to oceanhillman in the changelog entry

--- app/assets/data/changelog.ts
+++ app/assets/data/changelog.ts
@@ -368,7 +368,10 @@ export const CHANGELOG = (): ChangeLogEntry[] => [
             time: '4:36 PM'
         },
         title: 'List View Feature for heroes',
-        description: `Adds a new way to view your proficiencies all at once. On the Heroes page, there's now an option to toggle between "Gallery View" and "List View". Gallery View shows the hero cards as before. List View shows all of your heroes and their proficiency data as a table.`,
+        description: `Adds a new way to view your proficiencies all at once. On the Heroes page, there's now an option to toggle between "Gallery View" and "List View". Gallery View shows the hero cards as before. List View shows all of your heroes and their proficiency data as a table.
+        <br>
+        <br>
+        Thanks to <a href="https://github.com/oceanhillman">oceanhillman</a> for making this feature!`,
         listTitle: 'Features',
         list: [
             'Switch between Gallery View and List View on the Heroes Page',
